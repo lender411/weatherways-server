682f29cb1f83ad86ad9e40234677db25c279994c
Nicholas Brinkley <brinkleynicholas@yahoo.com>
2020-04-14 13:58:53 -0500

    trying to fix search in  MarkersModel.ts

--- src/controllers/commands/models/entities/MarkersModel.ts
+++ src/controllers/commands/models/entities/MarkersModel.ts
@@ -93,7 +93,7 @@ export const queryAll = async (): Promise<MarkersModel[]> => {
 export const searchAll = async (query: string): Promise<MarkersModel[]> => {
 	return MarkersModel.findAll({
 		where: {
-			id: { [Op.like]: query}
+			id: { id : query}
 		}
 	});
 };
