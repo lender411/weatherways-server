2e0f2fece60e484cb2d450820834a62f518c6733
Audrey Timmerman <audtimmerman@gmail.com>
2020-04-28 12:49:40 -0500

    properly use promise

--- src/controllers/commands/markers/MarkerCreateCommand.ts
+++ src/controllers/commands/markers/MarkerCreateCommand.ts
@@ -38,20 +38,22 @@ export const execute = async (saveMarkersRequest: MarkersSaveRequest): Promise<C
 	const openWeatherKey = "80f0f7a1ea95a376129420c77fe45bb9";
 	const url = `http://api.openweathermap.org/data/2.5/weather?lat=${saveMarkersRequest.Latitude}&lon=${saveMarkersRequest.Longitude}&appid=${openWeatherKey}&units=imperial`;
 
-	const json = new Promise((resolve, reject) => {
+	const promise1 = new Promise((resolve, reject) => {
 		request(url, (err, response, body) => {
 			if (err) {
 				console.log("error:", err);
 				reject(err);
 			} else {
 				resolve(body);
+				json = body
 				console.log("\n\nbody:", body);
 			}
 		});
 	});
 
-	console.log("\n\njson:", json);
-	const weather = JSON.parse(json);
+	promise1.then((json) => {
+		console.log("\n\njson:", json);
+	});
 
 	const markerToCreate: MarkersModel = <MarkersModel>{
 		id:saveMarkersRequest.id,
