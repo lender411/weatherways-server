e3ceaf09d30f9b2b5863b7b94c26b32b4b8b3b9b
Nicholas Brinkley <brinkleynicholas@yahoo.com>
2020-04-27 19:05:42 -0500

    attempting to integrate audrey's code

--- src/controllers/commands/markers/MarkerUpdateCommand.ts
+++ src/controllers/commands/markers/MarkerUpdateCommand.ts
@@ -46,7 +46,7 @@ export const execute = async (saveMarkersRequest: MarkersSaveRequest): Promise<C
 			}
 			const request = require("request");
 			const openWeatherKey = "80f0f7a1ea95a376129420c77fe45bb9";
-			const url = `http://api.openweathermap.org/data/2.5/weather?lat=${saveMarkersRequest.Latitude}&lon=${saveMarkersRequest.Longitude}&appid=${openWeatherKey}`;
+			const url =`http://api.openweathermap.org/data/2.5/weather?lat=${saveMarkersRequest.Latitude}&lon=${saveMarkersRequest.Longitude}&appid=${openWeatherKey}&units=imperial`;
 			let json = "";
 
 			request(url, function (err, response, body) {
